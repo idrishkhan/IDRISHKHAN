98f3b4bb7f020c3961e1f3fcb0cbff9c1edc5b35
idrishkhan <42800306+idrishkhan@users.noreply.github.com>
2018-09-26 14:44:52 +0530

    Update idrish set-3 2.py

--- idrish set-3 2.py
+++ idrish set-3 2.py
@@ -1,8 +1,6 @@
 lst = []
 num = int(input())
-print(num)
 for n in range(num):
-    numbers = int(input())
-    print(numbers,end=' ')
+    numbers = input()
     lst.append(numbers)
-print("\n",max(lst))
+print(max(lst))
